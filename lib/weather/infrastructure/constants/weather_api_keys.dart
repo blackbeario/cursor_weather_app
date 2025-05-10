f82874b517b51f260ab3489002c7bdf892234c4e
Jeff Frazier <jfrazdev@gmail.com>
2025-05-10 01:27:38 -0400

    Comment in api keys

--- lib/weather/infrastructure/constants/weather_api_keys.dart
+++ lib/weather/infrastructure/constants/weather_api_keys.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class WeatherApiKeys {
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
-  static String apiKey = dotenv.get('WEATHER_KEY');
+  static String apiKey = dotenv.get('WEATHER_KEY'); // Don't do this in production apps
 
   static String getWeatherByCity(String cityName) {
     return '$baseUrl/weather?q=$cityName&appid=$apiKey&units=imperial';
