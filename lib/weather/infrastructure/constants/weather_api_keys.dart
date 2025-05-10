0bad4ec8abe56fae51bc00121b6b57e22a2cb89c
Jeff Frazier <jfrazdev@gmail.com>
2025-05-10 00:59:21 -0400

    Fix json parsing errors

--- lib/weather/infrastructure/constants/weather_api_keys.dart
+++ lib/weather/infrastructure/constants/weather_api_keys.dart
@@ -1,6 +1,6 @@
 class WeatherApiKeys {
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
-  static const String apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
+  static const String apiKey = 'bb148e13e57a1b139bd596b34bc43a5b'; // Replace with your OpenWeatherMap API key
 
   static String getWeatherByCity(String cityName) {
     return '$baseUrl/weather?q=$cityName&appid=$apiKey&units=imperial';
